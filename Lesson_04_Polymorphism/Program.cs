7213eb979834a3cd7c865e66cc9c376e44c71973
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-05 16:47:17 +0700

    Add project files.

---
+++ Lesson_04_Polymorphism/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_04_Polymorphism
+{
+    class MedicalRecords
+    {
+        private string strPatientName;
+        private float fltMedicalFees;
+        public MedicalRecords(string strPatientName, float fltMedicalFees)
+        {
+            this.strPatientName = strPatientName; //this.strPatientName keyword <this> indicate that the name is refer private variable 
+            this.fltMedicalFees = fltMedicalFees; 
+        }
+        public float CelMedicalFees()
+        {
+            return fltMedicalFees;
+        }
+        public float CalMedicalFees(string test)
+        {
+            if (test =="Hapatities")
+            {
+                fltMedicalFees = fltMedicalFees + 17.7f;
+            }
+            else if (test =="Cholesterol")
+            {
+                fltMedicalFees = fltMedicalFees + 25f;
+            }
+            else //if (test == "Diabetes")
+            {
+                fltMedicalFees = fltMedicalFees + 20f;
+            }
+            return fltMedicalFees;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
